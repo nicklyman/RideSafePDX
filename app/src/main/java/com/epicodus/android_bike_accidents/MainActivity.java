f02cc6f6003c408a4dff57c8813d67a236d28ee4
Kevin Deganos and Nick Lyman <nick@>
2016-07-25 09:34:52 -0700

    Initial commit and files

---
+++ app/src/main/java/com/epicodus/android_bike_accidents/MainActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.android_bike_accidents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
