88fa51db0674399aa87da11ee8b4b01ab2162b57
Daniel Wulff <danielwulff.ee@gmail.com>
2016-07-25 10:15:56 -0700

    refactored

---
+++ app/src/main/java/com/epicodus/android_bike_accidents/Constants.java
@@ -0,0 +1,7 @@
+package com.epicodus.android_bike_accidents;
+
+/**
+ * Created by Guest on 7/25/16.
+ */
+public class Constants {
+}
--- app/src/main/java/com/epicodus/android_bike_accidents/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/android_bike_accidents/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.epicodus.android_bike_accidents;
+package com.epicodus.android_bike_accidents.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.epicodus.android_bike_accidents.R;
 
 public class MainActivity extends AppCompatActivity {
 
