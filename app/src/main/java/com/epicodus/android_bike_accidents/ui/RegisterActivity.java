f7a837a06e8b4104534f972fa26e834dfe509716
Kevin Deganos and Nick Lyman <nick@>
2016-07-25 10:41:50 -0700

    Add Register and Login activities with styling. Enable login with email/password settings in Firebase.

---
+++ app/src/main/java/com/epicodus/android_bike_accidents/ui/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.android_bike_accidents.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.android_bike_accidents.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
