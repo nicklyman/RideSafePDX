33d06a3d817643e7d09b7c00fc2fa338be030f7f
Kevin Deganos and Nick Lyman <nick@>
2016-07-25 15:11:36 -0700

    Added summary text, list and map views for accident buttons, and button to go to create report page.

--- app/src/main/java/com/epicodus/android_bike_accidents/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/android_bike_accidents/ui/MainActivity.java
@@ -2,14 +2,30 @@ package com.epicodus.android_bike_accidents.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epicodus.android_bike_accidents.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.summaryTextView) TextView mSummaryTextView;
+    @Bind(R.id.accidentListButton) Button mAccidentListButton;
+    @Bind(R.id.createReportButton) Button mCreateReportButton;
+    @Bind(R.id.accidentMapButton) Button mAccidentMapButton;
+    @Bind(R.id.bikeImageView) ImageView mBikeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Picasso.with(this).load(R.drawable.bike).into(mBikeImageView);
     }
 }
